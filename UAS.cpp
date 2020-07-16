c19f51261e9f6e05b5b6eab82819472094a503ae
FEARZA_KOSHI <53259263+FEARZA@users.noreply.github.com>
2020-07-16 14:43:01 +0800

    Update UAS.cpp

--- UAS.cpp
+++ UAS.cpp
@@ -2,27 +2,10 @@
 using namespace std;
 void rumus (){
 	float tp, d, tb, uk;
-	
-	cout << "Masukan total pembelian = ";
-	cin >> tp;
-	d = 0;
-	if (tp >= 500000){
-		d = 0.1 * tp;
-	}
-	
-	tb = tp - d;
-
+	cout << "Masukan total pembelian = "; cin >> tp; d = 0;
+	if (tp >= 500000){ d = 0.1 * tp; } tb = tp - d;
 	cout << "\n\nTotal diskon		= " << d  << "\n\nTotal belanja		= " << tb << "\n\nMasukan uang konsumen	= "; cin >> uk; cout << "\n\nuang kembalian		= " << uk - tb;
-	
-	if (tb >= uk){
-		cout << "Uangmu kurang bro...";
-		system ("color 4");
-	}else{
-		cout << "Terima kasih bro...";
-		system ("color 3");
-	}
-}
-int main(){
-	
-	rumus();
-}
+	if (tb >= uk){ cout << "Uangmu kurang bro..."; system ("color 4"); }
+	else{ cout << "Terima kasih bro..."; system ("color 3"); } }
+
+int main(){ rumus(); }
