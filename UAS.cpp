ba23b143d70186687ddd59a8e1c72fe042a162af
FEARZA_KOSHI <53259263+FEARZA@users.noreply.github.com>
2020-07-16 14:41:31 +0800

    Update UAS.cpp

--- UAS.cpp
+++ UAS.cpp
@@ -1,27 +1,20 @@
 #include <iostream>
 using namespace std;
 void rumus (){
-	float total_pembelian, diskon, total_belanja, uang_konsumen;
+	float tp, d, tb, uk;
 	
 	cout << "Masukan total pembelian = ";
-	cin >> total_pembelian;
-	diskon = 0;
-	if (total_pembelian >= 500000){
-		diskon = 0.1 * total_pembelian;
+	cin >> tp;
+	d = 0;
+	if (tp >= 500000){
+		d = 0.1 * tp;
 	}
 	
-	total_belanja = total_pembelian - diskon;
+	tb = tp - d;
+
+	cout << "\n\nTotal diskon		= " << d  << "\n\nTotal belanja		= " << tb << "\n\nMasukan uang konsumen	= "; cin >> uk; cout << "\n\nuang kembalian		= " << uk - tb;
 	
-	cout << endl;
-	cout << "Total diskon		= " << diskon;
-	cout << endl;
-	cout << "Total belanja		= " << total_belanja;
-	cout << endl;
-	cout << "Masukan uang konsumen	= ";
-	cin >> uang_konsumen;
-	cout << "uang kembalian		= " << uang_konsumen - total_belanja << endl;
-	
-	if (total_belanja >= uang_konsumen){
+	if (tb >= uk){
 		cout << "Uangmu kurang bro...";
 		system ("color 4");
 	}else{
